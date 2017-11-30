68489c347984ff7d75fa42096feea85ffcfb6980
christophe <chris.l.honore@gmail.com>
2017-11-30 16:14:08 +0100

    owner can add a new item + fix no decoration in navbar

--- app/controllers/owner/xmas_items_controller.rb
+++ app/controllers/owner/xmas_items_controller.rb
@@ -1,4 +1,34 @@
 class Owner::XmasItemsController < ApplicationController
   def index
   end
+
+  def show
+    @xmas_item
+
+  end
+
+  def new
+    @xmas_item = XmasItem.new
+  end
+
+  def create
+    @xmas_item = XmasItem.new(xmas_item_params)
+    @xmas_item.user = current_user
+    if @xmas_item.save
+      redirect_to xmas_items_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def xmas_item_params
+    params.require(:xmas_item).permit(:name, :description, :address, :photo, :price_per_day)
+  end
+
 end
+
+
+
+
