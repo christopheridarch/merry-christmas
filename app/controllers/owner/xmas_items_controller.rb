74c6a7c097532dbaa203999d903f5f9b6e762fab
Benjamin Moutel <benjoutel@gmail.com>
2017-11-30 15:38:40 +0100

    add reservations controller and owner item views

--- app/controllers/owner/xmas_items_controller.rb
+++ app/controllers/owner/xmas_items_controller.rb
@@ -1,4 +1,25 @@
 class Owner::XmasItemsController < ApplicationController
   def index
   end
+
+  def show
+    @xmas_item  = XmasItem.find(params[:id])
+  end
+
+  # def accept
+  #   @xmas_item.status = "Accepted"
+  #   redirect_to @xmas_item
+  # end
+
+  # def decline
+  #   @xmas_item.status = "Declined"
+  #   redirect_to @xmas_item
+  # end
+
+  private
+
+  # def xmas_item_params params.require(:xmas_item).permit(:status)
+  # end
 end
+
+
